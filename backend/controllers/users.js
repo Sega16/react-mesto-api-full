f6a0825b01c00a74ca3c5b1956148ba98416b816
Sergey Polozov <fiyzor-99@yandex.ru>
2022-09-02 00:43:55 +0500

    backend jwt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -89,7 +89,7 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'secret',
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
       res.cookie('jwt', token, {
